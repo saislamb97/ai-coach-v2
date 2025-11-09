29d6d1c3264e9d124b62f225392919fd24631781
Saiful Islam <sa.islamb97@gmail.com>
2025-11-10 03:53:37 +0800

    updated

--- note.sh
+++ note.sh
@@ -2,4 +2,4 @@ python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.
 
 cd app && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py collectstatic --noinput --clear
 
-mkdir -p temp/logs && cd app && celery -A aicoach worker -l info --logfile=../temp/logs/celery_worker.log & celery -A aicoach beat -l info --logfile=../temp/logs/celery_beat.log & python3 manage.py runserver 0.0.0.0:8000
+cd app && celery -A aicoach worker -l info & cd app && celery -A aicoach beat -l info & cd app && python3 manage.py runserver 0.0.0.0:8000
